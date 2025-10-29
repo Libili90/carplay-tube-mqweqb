26642da26eaa2ad92fd2dddf718b5df368cceb12
Libili90 <117081825+Libili90@users.noreply.github.com>
2025-10-29 18:47:53 +0100

    CarPlaySceneDelegate.h

---
+++ ios/CarPlayTube/CarPlaySceneDelegate.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+#import <CarPlay/CarPlay.h>
+
+@interface CarPlaySceneDelegate : UIResponder <CPTemplateApplicationSceneDelegate>
+
+@property (nonatomic, strong) CPInterfaceController *interfaceController;
+
+@end
